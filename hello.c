fb4d724d3cdb9d852ee958936cb0e44dc37d548f
lexlinhardt <42552486+lexlinhardt@users.noreply.github.com>
2018-08-23 16:22:27 -0500

    Added my name

--- hello.c
+++ hello.c
@@ -1,5 +1,5 @@
 /**
- * Author: Jack Keating		
+ * Author: Jack Keating and Alexis Linhardt		
  * Date: 2018/08/23
  *
  * A simple hello world program in C
@@ -10,7 +10,7 @@
 
 int main(int argc, char **argv) {
 
-  printf("Hello Jack Keating Mechanical Engineering\n");
+  printf("Hello Jack Keating Mechanical Engineering, Alexis Linhardt Computer Engineering\n");
 
   return 0;
 }
